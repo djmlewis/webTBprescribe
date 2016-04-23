69d255bd73548d7f5ab1dfdb140522706a4a04b0
davidlewis <djmlewis@hotmail.com>
2016-04-23 20:57:20 +0100

    q

--- prescribeTBsafe/ViewController.swift
+++ prescribeTBsafe/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        guard
+            let path = NSBundle.mainBundle().URLForResource("index", withExtension: "html", subdirectory: "site")
+            else
+        {
+            return
+        }
+        self.webView.loadRequest(NSURLRequest(URL: path))
+    }
 
 }
 
